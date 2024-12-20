3ea895e8090d57f18f2178469da66e1c5d20a134
Anderson Velásquez <andersonjosvela@gmail.com>
2024-12-19 18:18:01 -0600

    Definicion del esquema para Categorias

---
+++ models/categoryModel.js
@@ -0,0 +1,7 @@
+import { z } from 'zod'
+
+export const categorySchema = z.object({
+    nombre_categoria: z.string({message: 'La categoria debe de ser una cadena de texto'}).min(3).max(50)
+})
+
+export const updateCategorySchema = categorySchema.partial()
