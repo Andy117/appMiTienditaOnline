2442d1dc7df09127262489204afcedf7a30dee5b
Anderson Velásquez <andersonjosvela@gmail.com>
2024-12-19 21:50:29 -0600

    Se agregaron las rutas de Unidad de Medidas al API

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import routerLogin from './routes/authRoutes.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import brandRoutes from './routes/brandRoutes.js'
 import presentationRoutes from './routes/presentationRoutes.js'
+import measureRoutes from './routes/measureRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -18,6 +19,7 @@ app.use('/api/products', productRoutes)
 app.use('/api/categories', categoryRoutes)
 app.use('/api/brands', brandRoutes)
 app.use('/api/presentations', presentationRoutes)
+app.use('/api/measures', measureRoutes)
 
 //testing my db connection
 sequelize.authenticate()
