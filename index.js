d944a6f62d7f3d6ebb72710eac56f06e4725e0ba
Anderson Velásquez <andersonjosvela@gmail.com>
2024-12-19 18:18:26 -0600

    Se agrego el endpoint para Categorias

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import sequelize from './config/dbConfig.js'
 import 'dotenv/config'
 import productRoutes from './routes/productRoutes.js'
 import routerLogin from './routes/authRoutes.js'
+import categoryRoutes from './routes/categoryRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -12,6 +13,7 @@ app.use(express.json())
 //defining my routes
 app.use('/api', routerLogin)
 app.use('/api/products', productRoutes)
+app.use('/api/categories', categoryRoutes)
 
 //testing my db connection
 sequelize.authenticate()
