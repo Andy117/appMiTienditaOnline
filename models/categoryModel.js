e61814ede68e29eada8af552c3d68182664edfd1
Anderson Velásquez <andersonjosvela@gmail.com>
2024-12-20 21:54:51 -0600

    Se quito el partial() para que todo el cuerpo del JSON sea obligatorio

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -4,4 +4,4 @@ export const categorySchema = z.object({
     nombre_categoria: z.string({message: 'La categoria debe de ser una cadena de texto'}).min(3).max(50)
 })
 
-export const updateCategorySchema = categorySchema.partial()
+export const updateCategorySchema = categorySchema
