ead047fe6ab823ce20da71ad3ebf0c8613c52f84
Anderson Velásquez <andersonjosvela@gmail.com>
2024-12-30 22:16:07 -0600

    Se agrego una nueva ruta

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,13 @@
 import e from "express"
 import { verifyToken } from "../middleware/authMiddleware.js"
 import { authorizeRole } from "../middleware/roleMiddleware.js"
-import { createOrder, getAllOrdersWithDetails, updateOrder, updateOrderStatus } from "../controllers/orderController.js"
+import { createOrder, getAllOrdersWithDetails, getAllOrdersWithDetailsUsingID, updateOrder, updateOrderStatus } from "../controllers/orderController.js"
 
 const orderRoutes = e.Router()
 
 orderRoutes.get('/',verifyToken, authorizeRole(1), getAllOrdersWithDetails)
-orderRoutes.post('/', verifyToken, authorizeRole(1), createOrder)
+orderRoutes.get('/:id',verifyToken, authorizeRole(1), getAllOrdersWithDetailsUsingID)
+orderRoutes.post('/', verifyToken, createOrder)
 orderRoutes.put('/:id', verifyToken, authorizeRole(1), updateOrder)
 orderRoutes.patch('/:id', verifyToken, authorizeRole(1), updateOrderStatus)
 
