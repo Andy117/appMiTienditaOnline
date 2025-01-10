8f0b5d8104ac8b563641280c054152a1bd9fdddc
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:43:12 -0600

    Se agrego la funncion para Obtener todas las categorias por medio de paginacion, y tambien la funcion para activar una categoria, tambien una funcion para obtener todas las categorias

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,12 +2,41 @@ import sequelize from "../config/dbConfig.js"
 import { z } from 'zod'
 import { categorySchema, updateCategorySchema } from "../models/categoryModel.js"
 
-export const getAllCategories = async (req, res) => {
+export const getAllCategoriesPagination = async (req, res) => {
     try {
-        const [results] = await sequelize.query('SELECT * FROM CategoriaProductos')
-        res.json(results)
+        const page = parseInt(req.query.page, 10) || 1
+        const limit = parseInt(req.query.limit, 10) || 15
+        const offset = (page - 1) * limit
+
+        const [totalResults] = await sequelize.query(
+            'SELECT COUNT(*) AS total FROM CategoriaProductos'
+        )
+
+        const total = totalResults[0].total; 
+
+        const [categories] = await sequelize.query(
+            `SELECT * FROM CategoriaProductos ORDER BY idCategoriaProductos OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY`,
+            {
+                replacements: { offset, limit },
+            }
+        )
+        res.json({
+            categories,
+            total,
+            currentPage: page,
+            totalPages: Math.ceil(total / limit)
+        })
     } catch (error) {
         res.status(500).json({ message: 'Error al obtener las categorias...', error})
+    }
+}
+
+export const getAllCategories = async (req, res) => {
+    try {
+        const [categories] = await sequelize.query('SELECT * FROM CategoriaProductos')
+        res.json(categories)
+    } catch (error) {
+        res.statuts(500).json({ message: 'Hubo un error al obtener las categorias...', error})
     }
 }
 
@@ -113,6 +142,31 @@ export const deleteCategory = async (req, res) => {
         res.json({ message: 'Categoria eliminada/desactivada exitosamente' })
     } catch (error) {
         res.status(500).json({ message: 'Error al eliminar/desactivar la categoria...', error})
+
+    }
+}
+
+export const activateCategory = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const[category] = await sequelize.query(
+            'SELECT * FROM CategoriaProductos WHERE idCategoriaProductos = :id',
+            {replacements: { id }, type: sequelize.QueryTypes.SELECT}
+        )
+        
+        if(!category){
+            return res.status(404).json({ message: 'La categoria no existe o ya ha sido eliminada...'})
+        }
+
+        await sequelize.query('EXEC sp_ActivarCategoria @idCategoriaProducto=:id',
+            {
+                replacements: { id }
+            }
+        )
+        res.json({ message: 'Categoria Activada exitosamente' })
+    } catch (error) {
+        res.status(500).json({ message: 'Error al Activar la categoria...', error})
 
     }
 }
