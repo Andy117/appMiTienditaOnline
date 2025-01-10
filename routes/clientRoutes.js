e4302e44c45d9c191bde000f997338019bd7e148
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:51:38 -0600

    Se agrego una ruta para obtener clientes utilizando paginacion y tambien para activar a los clientes utilizando su id.

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,13 +1,15 @@
 import e from "express"
 import { verifyToken } from "../middleware/authMiddleware.js"
-import { createClient, deleteClient, getAllClients, updateClient } from "../controllers/clientController.js"
+import { activateClient, createClient, deleteClient, getAllClients, getAllClientsPaginated, updateClient } from "../controllers/clientController.js"
 import { authorizeRole } from "../middleware/roleMiddleware.js"
 
 const clientRoutes = e.Router()
 
 clientRoutes.get('/', verifyToken, authorizeRole(1), getAllClients)
+clientRoutes.get('/allPagination', verifyToken, authorizeRole(1), getAllClientsPaginated)
 clientRoutes.post('/', verifyToken, authorizeRole(1), createClient)
 clientRoutes.put('/:id', verifyToken, authorizeRole(1), updateClient)
 clientRoutes.patch('/:id', verifyToken, authorizeRole(1), deleteClient)
+clientRoutes.patch('/activate/:id', verifyToken, authorizeRole(1), activateClient)
 
 export default clientRoutes
