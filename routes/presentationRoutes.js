71fa026624150fe4b7beb1aedbc3b829f933f4af
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:53:57 -0600

    Se agrego una ruta para obtener todas las presentaciones utilizando paginacion. tambien se agrego una ruta para activar las presentaciones que esten desactivadas.

--- routes/presentationRoutes.js
+++ routes/presentationRoutes.js
@@ -1,13 +1,15 @@
 import e from "express"
 import { verifyToken } from "../middleware/authMiddleware.js"
-import { createPresentation, deletePresentation, getAllPresentations, updatePresentation } from "../controllers/presentationController.js"
+import { activatePresentation, createPresentation, deletePresentation, getAllPresentationPagination, getAllPresentations, updatePresentation } from "../controllers/presentationController.js"
 import { authorizeRole } from "../middleware/roleMiddleware.js"
 
 const presentationRoutes = e.Router()
 
 presentationRoutes.get('/', verifyToken, getAllPresentations)
+presentationRoutes.get('/allPagination', verifyToken, authorizeRole(1), getAllPresentationPagination)
 presentationRoutes.post('/', verifyToken, authorizeRole(1), createPresentation)
 presentationRoutes.put('/:id', verifyToken, authorizeRole(1), updatePresentation)
 presentationRoutes.patch('/:id', verifyToken, authorizeRole(1), deletePresentation)
+presentationRoutes.patch('/activate/:id', verifyToken, authorizeRole(1), activatePresentation)
 
 export default presentationRoutes
