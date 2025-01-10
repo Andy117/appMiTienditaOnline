d97ca6b99c0862d424698ebe53e58b2699fbcd71
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:52:00 -0600

    se agrego una ruta para obtener a las unidades de medida por paginacion y tambien para activar una unidad de medida utilzando su id

--- routes/measureRoutes.js
+++ routes/measureRoutes.js
@@ -1,13 +1,15 @@
 import e from "express"
 import { verifyToken } from "../middleware/authMiddleware.js"
-import { createMeasure, deleteMeasure, getAllMeasure, updateMeasure } from "../controllers/measureController.js"
+import { activateMeasure, createMeasure, deleteMeasure, getAllMeasure, getAllMeasurePagination, updateMeasure } from "../controllers/measureController.js"
 import { authorizeRole } from "../middleware/roleMiddleware.js"
 
 const measureRoutes = e.Router()
 
 measureRoutes.get('/', verifyToken, getAllMeasure)
+measureRoutes.get('/allPagination', verifyToken, authorizeRole(1), getAllMeasurePagination)
 measureRoutes.post('/', verifyToken, authorizeRole(1), createMeasure)
 measureRoutes.put('/:id', verifyToken, authorizeRole(1), updateMeasure)
 measureRoutes.patch('/:id', verifyToken, authorizeRole(1), deleteMeasure)
+measureRoutes.patch('/activate/:id', verifyToken, authorizeRole(1), activateMeasure)
 
 export default measureRoutes
