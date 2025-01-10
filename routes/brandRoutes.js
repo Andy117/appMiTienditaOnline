dc54451534c369e531d099046c3763750b133275
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:50:52 -0600

    se agregaron las rutas para obtener marcas por paginacion y activar una marca.

--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -1,13 +1,15 @@
 import e from "express"
 import { verifyToken } from "../middleware/authMiddleware.js"
-import { createBrand, deleteBrand, getAllBrands, updateBrand } from "../controllers/brandController.js"
+import { activateBrand, createBrand, deleteBrand, getAllBrands, getAllBrandsPagination, updateBrand } from "../controllers/brandController.js"
 import { authorizeRole } from "../middleware/roleMiddleware.js"
 
 const brandRoutes = e.Router()
 
 brandRoutes.get('/', verifyToken, getAllBrands)
+brandRoutes.get('/allPagination', verifyToken, authorizeRole(1), getAllBrandsPagination)
 brandRoutes.post('/', verifyToken, authorizeRole(1), createBrand)
 brandRoutes.put('/:id', verifyToken, authorizeRole(1), updateBrand)
 brandRoutes.patch('/:id', verifyToken, authorizeRole(1), deleteBrand)
+brandRoutes.patch('/activate/:id', verifyToken, authorizeRole(1), activateBrand)
 
 export default brandRoutes
