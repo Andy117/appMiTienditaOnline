ce512087eb07e2c46ded68fea851a208361ac2dd
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:53:31 -0600

    se agregaron rutas para: - Obtener todas las ordenes para el cliente. - obtener todas las ordenes con sus respectivos detalles - obtener una orden y sus detalles utilizando su id. - cancelar una orden utilizando su id - Actualizar una orden y sus detalles utilizando su id

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,14 +1,17 @@
 import e from "express"
 import { verifyToken } from "../middleware/authMiddleware.js"
 import { authorizeRole } from "../middleware/roleMiddleware.js"
-import { createOrder, getAllOrdersWithDetails, getAllOrdersWithDetailsUsingID, updateOrder, updateOrderStatus } from "../controllers/orderController.js"
+import { cancelOrder, createOrder, getAllOrdersForClient, getAllOrdersWithDetails, getAllOrdersWithDetailsUsingID, updateOrder, updateOrderStatus, updateOrderWithDetails } from "../controllers/orderController.js"
 
 const orderRoutes = e.Router()
 
-orderRoutes.get('/',verifyToken, authorizeRole(1), getAllOrdersWithDetails)
-orderRoutes.get('/:id',verifyToken, authorizeRole(1), getAllOrdersWithDetailsUsingID)
+orderRoutes.get('/client',verifyToken, getAllOrdersForClient)
+orderRoutes.get('/',verifyToken, getAllOrdersWithDetails)
+orderRoutes.get('/:id',verifyToken, getAllOrdersWithDetailsUsingID)
+
 orderRoutes.post('/', verifyToken, createOrder)
-orderRoutes.put('/:id', verifyToken, authorizeRole(1), updateOrder)
+orderRoutes.put('/cancel/:id', verifyToken, cancelOrder)
+orderRoutes.put('/:id', verifyToken, authorizeRole(1), updateOrderWithDetails)
 orderRoutes.patch('/:id', verifyToken, authorizeRole(1), updateOrderStatus)
 
 export default orderRoutes
