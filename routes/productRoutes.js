6e30c684857691d35f3c9155adf2ad829f5d8ae3
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:55:14 -0600

    Se agregaron rutas para: - Obtener todos los productos del lado del operador. - obtener productos utilizando su id - actualizar un producto utilizando su id - activar un producto, utilizando su id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,18 @@
 import express from "express"
 import { verifyToken } from "../middleware/authMiddleware.js"
-import { createProduct, deleteProduct, getAllProducts, updateProduct } from "../controllers/productController.js"
-import { authorizeRole } from "../middleware/roleMiddleware.js";
-import multer from "multer";
+import { activateProduct, createProduct, deleteProduct, getAllProducts, getAllProductsOperator, getProductsById, updateProduct } from "../controllers/productController.js"
+import { authorizeRole } from "../middleware/roleMiddleware.js"
+import multer from "multer"
 
 const productRoutes = express.Router();
 const upload = multer({ storage: multer.memoryStorage() })
 
 productRoutes.get('/', verifyToken, getAllProducts)
+productRoutes.get('/operator', verifyToken, getAllProductsOperator)
+productRoutes.get('/:id', verifyToken, getProductsById)
 productRoutes.post('/', verifyToken, authorizeRole(1), upload.single('imagenProducto'), createProduct)
-productRoutes.put('/:id', verifyToken, authorizeRole(1), updateProduct)
+productRoutes.put('/:id', verifyToken, authorizeRole(1), upload.single('imagenProducto'), updateProduct)
 productRoutes.patch('/:id', verifyToken, authorizeRole(1), deleteProduct)
+productRoutes.patch('/activate/:id', verifyToken, authorizeRole(1), activateProduct)
 
 export default productRoutes
