d7916694703975d66a9624b550cccb774021ba9d
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:56:24 -0600

    Se agregaron rutas para: - Obtener todos los usuarios utilizando paginacion - crear un usuario - Activar un usuario inactivo - Cambiar a un usuario a administrador - Cambiar a un usuario a cliente - cambiar la contrasenia de un usuario

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,18 @@
 import e from "express"
 import { verifyToken } from "../middleware/authMiddleware.js"
 import { authorizeRole } from "../middleware/roleMiddleware.js"
-import { createUser, deleteUser, getAllUsers, updateUser } from "../controllers/userController.js"
+import { activateUser, changePassword, changeToAdmin, changeToClient, createUser, deleteUser, getAllUsers, getAllUsersPaginated, updateUser } from "../controllers/userController.js"
 
 const userRoutes = e.Router()
 
 userRoutes.get('/', verifyToken, authorizeRole(1), getAllUsers)
-userRoutes.post('/', verifyToken, authorizeRole(1), createUser)
+userRoutes.get('/allPagination', verifyToken, authorizeRole(1), getAllUsersPaginated)
+userRoutes.post('/', createUser)
 userRoutes.put('/:id', verifyToken, authorizeRole(1), updateUser)
 userRoutes.patch('/:id', verifyToken, authorizeRole(1), deleteUser)
+userRoutes.patch('/activate/:id', verifyToken, authorizeRole(1), activateUser)
+userRoutes.patch('/changeToAdmin/:id', verifyToken, authorizeRole(1), changeToAdmin)
+userRoutes.patch('/changeToClient/:id', verifyToken, authorizeRole(1), changeToClient)
+userRoutes.patch('/changePassword/:id', verifyToken, authorizeRole(1), changePassword)
 
 export default userRoutes
