f1afedc835c22b71fe12ac9c27880da467d2f1bf
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:51:14 -0600

    Se agrego la ruta para obtener todas las categorias utilizando paginacion y tambien para activar una categoria utilziando paginacion

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import { verifyToken } from '../middleware/authMiddleware.js'
-import { createCategory, deleteCategory, getAllCategories, updateCategory } from '../controllers/categoryController.js'
+import { activateCategory, createCategory, deleteCategory, getAllCategories, getAllCategoriesPagination, updateCategory } from '../controllers/categoryController.js'
 import { authorizeRole } from '../middleware/roleMiddleware.js'
 
 const categoryRoutes = express.Router()
 
 categoryRoutes.get('/', verifyToken, getAllCategories)
+categoryRoutes.get('/allPagination', verifyToken, getAllCategoriesPagination)
 categoryRoutes.post('/', verifyToken, authorizeRole(1), createCategory)
 categoryRoutes.put('/:id', verifyToken, authorizeRole(1), updateCategory)
 categoryRoutes.patch('/:id', verifyToken, authorizeRole(1), deleteCategory)
+categoryRoutes.patch('/activate/:id',verifyToken,authorizeRole(1),activateCategory)
 
 export default categoryRoutes
