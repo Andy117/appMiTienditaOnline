6e6186c5851899417e34d9c9988d37ff93e0e8c5
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:50:29 -0600

    Actualizacion de modelo de validacion de datos

--- models/orderModel.js
+++ models/orderModel.js
@@ -96,6 +96,48 @@ export const updateOrderSchema = z.object({
     correoElectronicoOrden: z.string({
         required_error: 'El correo electronico es obligatorio...',
         invalid_type_error: 'El correo electronico debe ser una cadena de texto...'
-    }).email({ message: 'El correo electronico debe de cumplir con el formato usuario@dominio.com' })
+    }).email({ message: 'El correo electronico debe de cumplir con el formato usuario@dominio.com' }),
+    fechaEntregaOrden: z.string({
+        required_error: 'La fecha de entrega es obligatoria...',
+        invalid_type_error: 'La fecha de entrega debe ser una cadena en formato YYYY-MM-DD'
+    }).date(),
+    totalOrden: z.number({
+        required_error: 'El total de la orden es obligatorio...',
+        invalid_type_error: 'El total de la orden debe de esr un numero...',
+    }).positive({message: 'el total de la orden debe de esr mayor a 0...'}),
+    DetallesJSON: z.array(z.object({
+        idProducto: z.number({
+            required_error: 'El ID del producto es obligatorio...',
+            invalid_type_error: 'El ID debe de ser un numero'
+        }).int().positive({message: 'El ID debe de ser mayor a 0...'}),
+        cantidad: z.number({
+            required_error: 'La cantidad de productos es obligatoria...',
+            invalid_type_error: 'La cantidad debe de ser un numero...'
+        }).int().positive({message: 'La cantidad debe de ser un numero mayor a 0...'}),
+        precio: z.number({
+            required_error: 'El precio del producto es obligatorio...',
+            invalid_type_error: 'El precio del producto debe de ser un numero...'
+        }).positive({message: 'El precio debe de ser mayor a 0'})
+    })).min(1, {message: 'Debe haber al menos un detalle en la orden...'}),
+}).superRefine((data, ctx) => {
+    const currentDate = new Date()
+    const deliveryDate = new Date(data.fechaEntregaOrden)
+
+    //Validating the date we have to deliver the order !< current date
+    if(deliveryDate < currentDate){
+        ctx.addIssue({
+            path: ['fechaEntregaOrden'],
+            message: 'La fecha de entrega no puede ser anterior a la fecha actual...',
+        })
+    }
+
+    //Validating the totalOrden matches subTotals of the products
+    const totalCalculado = data.DetallesJSON.reduce((sum, item) => sum + item.cantidad * item.precio, 0)
+    if(totalCalculado !== data.totalOrden){
+        ctx.addIssue({
+            path: ['totalOrden'],
+            message: `El totalOrden (${data.totalOrden}) no coincide con la suma de los productos (${totalCalculado.toFixed(2)}))`
+        })
+    }
 })
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,6 @@
 import { z } from 'zod'
 
 export const userSchema = z.object({
-    rol_idRol: z.number().int({
-        required_error: "El rol es obligatorio...",
-        invalid_type_error: "El rol debe ser 1:Operador, 2:Cliente",
-    }).min(1, { message: 'El rol debe ser 1:Operador, 2:Cliente'}).max(2, { message: 'El rol debe ser 1:Operador, 2:Cliente'}),
-    Clientes_idClientes: z.number({message:'El id Cliente debe de ser un numero'}).int().positive({message: 'Solo se permiten valores positivos...'}),
     nombre_completo: z.string({message: 'El nombre debe de ser una cadena de texto'}),
     correo_electronico: z.string({
         required_error: 'El correo electronico es obligatorio...',
@@ -26,17 +21,21 @@ export const userSchema = z.object({
     }).length(8, {
         invalid_type_error: 'El numero de telefono debe de contener 8 digitos...'
     }).trim(),
-    fecha_nacimiento: z.string().date({
+    fecha_nacimiento: z.string({
         required_error: 'La fecha de nacimiento es obligatoria...',
-        invalid_type_error: 'La fecha de nacimiento debe de ingresarse en formato fecha YYYY-MM-DD'
-    }).max(new Date('2005-01-01'),{
-        invalid_type_error: 'La fecha de nacimiento maxima aceptada es: 2005-01-01'
-    }).min(new Date('1900-01-01'),{
-        invalid_type_error: 'La fecha mas antigua aceptada es: 1900-01-01'
-    })
+        invalid_type_error: 'Error en el tipo de datos enviados se especha formato Fecha'
+    }).max(new Date(new Date().setFullYear(new Date().getFullYear() - 18)), 'Debes tener almenos 18 años para registrarte').transform((value) => (value ? new Date(value) : null))
 })
 
 export const updateUserSchema = userSchema.partial({
-    Clientes_idClientes: true,
     fecha_nacimiento: true
+})
+
+export const updatePasswordSchema = z.object({
+    contrasenia: z.string({
+        required_error: 'La contrasenia es obligatoria...',
+        invalid_type_error: 'La contrasenia debe de ser una cadena de texto...'
+    }).min(8, {
+        message: 'La contrasenia debe de tener almenos 8 caracteres...'
+    })
 })
