ea55a7878446a59a58e47ed754383fef393a4db8
Anderson Velásquez <andersonjosvela@gmail.com>
2025-01-09 20:45:42 -0600

    Se agrego la funcion para actualizar una orden y tambien sus detalles utilizando el id de la orden como parametro, tambien, se agrego una funcion para obteenr todas las ordenes para el cliente.
    Por ultimo se agrego una funcion para cancelar una orden

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -26,7 +26,7 @@ export const getAllOrdersWithDetails = async (req, res) => {
                 subtotal,
                 idEstados,
                 Estado_De_La_Orden
-            } = row;
+            } = row
 
             if (!ordersMap.has(OrdenID)) {
                 ordersMap.set(OrdenID, {
@@ -40,7 +40,7 @@ export const getAllOrdersWithDetails = async (req, res) => {
                     fecha_entrega,
                     total_orden,
                     DetallesOrden: []
-                });
+                })
             }
 
             ordersMap.get(OrdenID).DetallesOrden.push({
@@ -51,8 +51,8 @@ export const getAllOrdersWithDetails = async (req, res) => {
                 ProductoPrecio,
                 cantidad,
                 subtotal
-            });
-        });
+            })
+        })
 
         const orders = Array.from(ordersMap.values());
 
@@ -66,9 +66,9 @@ export const getAllOrdersWithDetails = async (req, res) => {
             success: false,
             message: 'Error al obtener las órdenes con sus detalles.',
             error: error.message,
-        });
+        })
     }
-};
+}
 
 export const getAllOrdersWithDetailsUsingID = async (req, res) => {
     const { id } = req.params;
@@ -80,10 +80,10 @@ export const getAllOrdersWithDetailsUsingID = async (req, res) => {
         );
 
         if (!results || results.length === 0) {
-            return res.status(404).json({ message: 'La Orden no existe o ya ha sido eliminada...' });
+            return res.status(404).json({ message: 'La Orden no existe o ya ha sido eliminada...' })
         }
 
-        const ordersMap = new Map();
+        const ordersMap = new Map()
 
         results.forEach(row => {
             const {
@@ -103,9 +103,8 @@ export const getAllOrdersWithDetailsUsingID = async (req, res) => {
                 subtotal,
                 EstadoID,
                 Estado_De_La_Orden
-            } = row;
+            } = row
 
-            // Verifica si la orden ya existe en el mapa
             if (!ordersMap.has(OrdenID)) {
                 ordersMap.set(OrdenID, {
                     OrdenID,
@@ -117,11 +116,9 @@ export const getAllOrdersWithDetailsUsingID = async (req, res) => {
                     correo_electronico,
                     fecha_entrega,
                     total_orden,
-                    DetallesOrden: [] // Inicializa el array de detalles
-                });
+                    DetallesOrden: []
+                })
             }
-
-            // Agrega el detalle a la orden correspondiente
             ordersMap.get(OrdenID).DetallesOrden.push({
                 DetalleID,
                 ProductoID,
@@ -130,10 +127,10 @@ export const getAllOrdersWithDetailsUsingID = async (req, res) => {
                 ProductoPrecio,
                 cantidad,
                 subtotal
-            });
-        });
+            })
+        })
 
-        const orders = Array.from(ordersMap.values());
+        const orders = Array.from(ordersMap.values())
 
         return res.status(200).json({
             success: true,
@@ -145,10 +142,9 @@ export const getAllOrdersWithDetailsUsingID = async (req, res) => {
             success: false,
             message: 'Error al obtener las órdenes con sus detalles.',
             error: error.message,
-        });
+        })
     }
-};
-
+}
 
 export const createOrder = async (req, res) => {
     try {
@@ -214,6 +210,47 @@ export const updateOrder = async(req, res) => {
     }
 }
 
+export const updateOrderWithDetails = async(req, res) => {
+    const { id } = req.params
+    try {
+        const[order] = await sequelize.query(
+            'SELECT * FROM Orden WHERE idOrden = :id',
+            {
+                replacements: { id }, type: sequelize.QueryTypes.SELECT
+            }
+        )
+
+        if(!order){
+            return res.status(404).json({ message: 'La orden con ese ID no existe o ya ha sido eliminada' })
+        }
+
+        const validatedData = updateOrderSchema.parse(req.body)
+        const detallesJSON = JSON.stringify(validatedData.DetallesJSON)
+        
+        await sequelize.query(
+            'EXEC sp_ActualizarOrdenConDetalles @idOrden=:id, @nombreCompleto=:nombreCompleto, @direccionOrden=:direccionOrden, @telefonoOrden=:telefonoOrden, @correoElectronicoOrden=:correoElectronicoOrden, @fechaEntregaOrden=:fechaEntregaOrden, @totalOrden=:totalOrden, @DetallesJSON=:DetallesJSON',
+            {
+                replacements:{
+                    nombreCompleto: validatedData.nombreCompleto,
+                    direccionOrden: validatedData.direccionOrden,
+                    telefonoOrden: validatedData.telefonoOrden,
+                    correoElectronicoOrden: validatedData.correoElectronicoOrden,
+                    fechaEntregaOrden: validatedData.fechaEntregaOrden,
+                    totalOrden: validatedData.totalOrden,
+                    DetallesJSON: detallesJSON,
+                    id
+                }
+            }
+        )
+        res.json({ message: 'Datos de la orden actualizados con exito!!!!'})
+    } catch (error) {
+        if(error instanceof z.ZodError){
+            return res.status(400).json({ errors: error.errors})
+        }
+        res.status(500).json({ message: 'Hubo un problema al actualizar los datos de la orden: ', error: error.message})
+    }
+}
+
 export const updateOrderStatus = async (req, res) => {
     const { id } = req.params
     try {
@@ -248,3 +285,98 @@ export const updateOrderStatus = async (req, res) => {
         res.status(500).json({ message: 'Hubo un problema al actualizar el estado de la orden...', error })
     }
 }
+
+export const getAllOrdersForClient = async (req, res) => {
+    const { idUsuario } = req.user
+
+    try {
+        const results = await sequelize.query('EXEC sp_ObtenerOrdenesPorCliente @idUsuarios=:idUsuario', {
+            replacements: { idUsuario },
+            type: sequelize.QueryTypes.SELECT,
+        });
+
+        if (!results || results.length === 0) {
+            return res.status(404).json({ message: 'No se encontraron órdenes para este cliente.' });
+        }
+
+        const ordersMap = new Map();
+
+        results.forEach(row => {
+            const {
+                OrdenID,
+                nombre_completo,
+                direccion,
+                telefono,
+                correo_electronico,
+                fecha_entrega,
+                total_orden,
+                DetalleID,
+                ProductoID,
+                ProductoNombre,
+                ProductoDescripcion,
+                ProductoPrecio,
+                cantidad,
+                subtotal,
+                EstadoID,
+                Estado_De_La_Orden,
+            } = row;
+
+            if (!ordersMap.has(OrdenID)) {
+                ordersMap.set(OrdenID, {
+                    OrdenID,
+                    EstadoID,
+                    Estado_De_La_Orden,
+                    nombre_completo,
+                    direccion,
+                    telefono,
+                    correo_electronico,
+                    fecha_entrega: new Date(fecha_entrega).toLocaleDateString('es-ES'),
+                    total_orden,
+                    DetallesOrden: [],
+                });
+            }
+
+            ordersMap.get(OrdenID).DetallesOrden.push({
+                DetalleID,
+                ProductoID,
+                ProductoNombre,
+                ProductoDescripcion,
+                ProductoPrecio,
+                cantidad,
+                subtotal,
+            });
+        });
+
+        const orders = Array.from(ordersMap.values());
+
+        return res.status(200).json({
+            success: true,
+            data: orders,
+        });
+    } catch (error) {
+        console.error('Error al obtener las órdenes del cliente:', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Error al obtener las órdenes del cliente.',
+            error: error.message,
+        });
+    }
+}
+
+export const cancelOrder = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await sequelize.query(
+            'EXEC sp_CancelarOrden @idOrden=:id',
+            {
+                replacements: { id }
+            }
+        );
+        res.json({ message: 'Orden cancelada exitosamente' });
+    } catch (error) {
+        res.status(500).json({ 
+            message: 'Error al cancelar la orden',
+            error: error.message 
+        });
+    }
+};
